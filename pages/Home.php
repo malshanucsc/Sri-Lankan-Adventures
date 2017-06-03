31b644f5d7f1a37fb2ee8e24f17e88b2c1b77006
malshanucsc <randhajapvt@gmail.com>
2017-06-03 21:44:30 +0530

    with animating slides

---
+++ pages/Home.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/mycss.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap/css/bootstrap.min.css">
+	<script src="../javascript/myjs.js">
+</head>
+<body>
+	<div id="header" class="col-md-12">
+		<div class= "col-md-2 " id="logo">
+			<img   src="../images/logo.png" >
+		</div>
+		<div class="col-md-8" id="topic">
+			<h1>Sri Lankan Adventures </h1>
+		</div>
+	</div>	
+<div id="upbanner" class="col-md-12">
+	Looking for a <select >
+				  <option value="1">Whale Watching</option>
+				  <option value="2">Wild safari</option>
+				  <option value="3">Surfing</option>
+				  <option value="4">Diving</option>
+				  <option value="5">Sun bathing</option>
+				  <option value="6">Historical place visit</option>
+				  <option value="7">Hill country visit</option>
+				  <option value="8">Ayurvedic spa</option> 
+				  <option value="9">Meditation and Yoga</option>
+				  </select>
+	<input type="submit" id="search_button" value="search"/>
+	<br><br>
+</div>			
+<div class="slideshow-container">
+
+<div class="myslides fade">
+	<div class="numbertext">1/7</div>
+	<img src= "Sri-Lanka-Tourism-campaign-1.png"  style="width:100%">
+	<div class="text"><h3>Welcome to Sri Lanka</h3></div>
+ </div>
+ <div class="myslides fade">
+	<div class="numbertext">2/7</div>
+	<img src="tngalla.jpg" style="width:100%">
+	<div class="text"><h3>Climate variation</h3></div>
+</div>
+<div class="myslideds fade">
+	<div class="numbertext">3/7</div>
+	<img src="image4.jpg" style="width:100%">
+	<div class="text"><h3> Plenty of wild life </h3></div>
+</div>
+<div class="myslideds fade">
+	<div class="numbertext">4/7</div>
+	<img src="images.jpg" style="width:100%">
+	<div class="text"><h3> Sri lanakan culture </h3></div>
+</div>
+<div class="myslideds fade">
+	<div class="numbertext">5/7</div>
+	<img src="dambullacavetemple_3465946a-xlarge.jpg" style="width:100%">
+	<div class="text"><h3>Proud History </h3></div>
+</div>
+<div class="myslideds fade">
+	<div class="numbertext">6/7</div>
+	<img src="hillcountry.jpg" style="width:100%">
+	<div class="text"><h3>Sri Lankan Upper Hills </h3></div>
+</div>
+<div class="myslideds fade">
+	<div class="numbertext">5/7</div>
+	<img src="river.jpg" style="width:100%">
+	<div class="text"><h3>Plenty of Rivers</h3></div>
+</div>
+</div>
+
+<div style="text-align:right">
+	<span class="dot"></span>
+	<span class="dot"></span>
+	<span class="dot"></span>
+	<span class="dot"></span>
+	<span class="dot"></span>
+	<span class="dot"></span>
+	<span class="dot"></span>
+</div>
+</body>
+</html>
